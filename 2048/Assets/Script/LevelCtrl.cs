eb9808cd6e65f2b3d6e04759880e666d5eae2d51
jyzgo <8559509@163.com>
2016-03-05 10:43:54 +0800

    Init

---
+++ 2048/Assets/Script/LevelCtrl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelCtrl : MonoBehaviour {
+
+	public GameObject cell_1;
+	public GameObject cell_2;
+	public GameObject cell_4;
+	public GameObject cell_8;
+	public GameObject cell_16;
+	public GameObject cell_32;
+	public GameObject cell_64;
+	public GameObject cell_128;
+	public GameObject cell_256;
+	public GameObject cell_512;
+	public GameObject cell_1024;
+	public GameObject cell_2048;
+	public GameObject cell_4096;
+
+	public Transform Board;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
